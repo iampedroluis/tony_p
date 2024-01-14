cf5c3a009fabf7ffc732abb8df650f736da22964
iampedroluis <pedroluisgutierrez96@gmail.com>
2024-01-13 23:05:07 -0300

    se añade funcionalidad A MEDIAS  de editar un usuario

--- front/src/front/js/pages/Admin.jsx
+++ front/src/front/js/pages/Admin.jsx
@@ -3,8 +3,8 @@ import { Context } from "../store/appContext";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import { Button, Modal } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css"; 
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Swal from 'sweetalert2'
 
 
 
@@ -14,12 +14,13 @@ export const Admin = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(0);
   const usuariosPerPage = 3;
- const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [editUser, setEditUser] = useState(null);
-const [usuarios , setUsuarios] = useState([])
-  
+  const [usuarios, setUsuarios] = useState([])
+  const [roles, setRoles] = useState([])
+  const [password, setPassword] = useState("")
   // Puedes utilizar este array en lugar del array vacío que tenías en tu código.
-  
+
 
   const offset = currentPage * usuariosPerPage;
   const filteredUsuarios = usuarios.filter(
@@ -37,32 +38,12 @@ const [usuarios , setUsuarios] = useState([])
   };
 
   const [hoveredUserId, setHoveredUserId] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  const getRoleColor = (roleId) => {
-    switch (roleId) {
-      case 1:
-        return "admin";
-      case 2:
-        return "maestro";
-      case 3:
-        return "alumno";
-      case 4:
-        return "padre"
-      default:
-        return "otro";
-    }
-    
-  };
-
-  const roles = [
-    { id: 1, label: "Admin" },
-    { id: 2, label: "Maestro" },
-    { id: 3, label: "Alumno" },
-    {id:4, label: "Padres"}
-  ];
 
   const handleEdit = (user) => {
     setEditUser(user);
+    setConfirmPassword(user.password)
     setShowModal(true);
   };
 
@@ -72,9 +53,32 @@ const [usuarios , setUsuarios] = useState([])
   };
 
   const handleSaveChanges = () => {
+    if (password !== confirmPassword) {
+      // Muestra un mensaje de error indicando que las contraseñas no coinciden
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Las contraseñas no coinciden',
+      });
+      return;
+    }
+    const userToUpdate = {
+      nombre: editUser.nombre,
+      apellido: editUser.apellido,
+      email: editUser.email,
+      rol_id: editUser.rol_id,
+      password : editUser.password
+    };
+    if (password.trim() !== "") {
+      userToUpdate.password = password;
+    } else {
+      // Si la contraseña no cambió, asignar la contraseña inicial
+      userToUpdate.password = confirmPassword;
+    }
+  
     // Lógica para guardar los cambios del usuario editado
     // actions.editUser(editUser); // Asegúrate de tener una función editUser en tu store o actions
-    console.log("Usuario editado:", editUser);
+    console.log("Usuario editado:", JSON.stringify(userToUpdate) +  "el id del usuario es "+editUser.id);
     handleCloseModal();
   };
 
@@ -91,8 +95,67 @@ const [usuarios , setUsuarios] = useState([])
 
     // Llamada a la función asincrónica
     fetchUsuarios();
+    const fetchRoles = async () => {
+      try {
+        const data = await actions.getRoles()
+        if (data.success) {
+          setRoles(store.roles)
+          console.log(roles)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchRoles()
   }, [currentPage, actions.getUsuarios]);
 
+  const getRolById = (roleId) => {
+    const role = roles.find((r) => r.id === roleId);
+    return role ? role.rol : 'Desconocido';
+  };
+  const handleDelete = async (userId) => {
+    try {
+      const confirmation = await Swal.fire({
+        title: '¿Estás seguro?',
+        text: 'No podrás revertir esto',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Sí, eliminarlo'
+      });
+
+      if (confirmation.isConfirmed) {
+        const resp = await actions.deleteUser(userId);
+        if (resp.success) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `El Usuario se eliminó exitosamente`,
+            showConfirmButton: false,
+            timer: 2500
+          });
+
+          // Función asincrónica para obtener los usuarios
+          const fetchUsuarios = async () => {
+            try {
+              const data = await actions.getUsuarios();
+              setUsuarios(data);
+            } catch (error) {
+              console.error("Error fetching usuarios", error);
+            }
+          };
+
+          // Llamada a la función asincrónica
+          fetchUsuarios();
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+
+    }
+  };
 
   return (
     <div className="container h-screen mt-5">
@@ -115,7 +178,7 @@ const [usuarios , setUsuarios] = useState([])
                 setSearchTerm(e.target.value.trim());
               }}
             />
-            
+
           </div>
         </div>
 
@@ -128,13 +191,13 @@ const [usuarios , setUsuarios] = useState([])
                     <p className="dark:text-principal-white">Nombre</p>
                   </th>
                   <th className="bg-transparent" scope="col">
-                  <p className="dark:text-principal-white">Apellido</p>
+                    <p className="dark:text-principal-white">Apellido</p>
                   </th>
                   <th className="bg-transparent" scope="col">
-                  <p className="dark:text-principal-white">Email</p>
+                    <p className="dark:text-principal-white">Email</p>
                   </th>
                   <th className="bg-transparent" scope="col">
-                  <p className="dark:text-principal-white">Rol</p>
+                    <p className="dark:text-principal-white">Rol</p>
                   </th>
                   <th className="bg-transparent" scope="col">
                     <p className="text-transparent">edit</p>
@@ -145,14 +208,13 @@ const [usuarios , setUsuarios] = useState([])
                 {currentUsuarios.map((usuario) => (
                   <tr
                     key={usuario.id}
-                    className={`bg-transparent ${
-                      hoveredUserId === usuario.id ? "hover:bg-gray-200" : ""
-                    }`}
+                    className={`bg-transparent ${hoveredUserId === usuario.id ? "hover:bg-gray-200" : ""
+                      }`}
                     onMouseEnter={() => setHoveredUserId(usuario.id)}
                     onMouseLeave={() => setHoveredUserId(null)}
                   >
                     <td className="bg-transparent" scope="row">
-                    <p className="dark:text-principal-white">{usuario.nombre}</p>
+                      <p className="dark:text-principal-white">{usuario.nombre}</p>
                     </td>
                     <td className="bg-transparent"><p className="dark:text-principal-white">{usuario.apellido}</p></td>
                     <td className="bg-transparent">
@@ -160,27 +222,24 @@ const [usuarios , setUsuarios] = useState([])
                     </td>
                     <td className="bg-transparent">
                       <p
-                        className={`text-white font-black inline-block rounded px-2 text-center capitalize ${
-                          usuario.rol_id === 1
-                            ? "bg-admin"
-                            : usuario.rol_id === 2
-                            ? "bg-maestro"
-                            : usuario.rol_id === 3
-                            ? "bg-alumno"
-                            :usuario.rol_id === 4
-                            ? "bg-padre"
-                            : "bg-otro"
-                        }`}
+                        className={`font-black inline-block rounded px-2 text-center capitalize text-dark-black  dark:text-principal-white`}
                       >
-                        {getRoleColor(usuario.rol_id)}
+                        {getRolById(usuario.rol_id)}
                       </p>
                     </td>
                     <td className="bg-transparent">
-                      {hoveredUserId === usuario.id && (
-                        <button onClick={() => handleEdit(usuario)}>
-                  <i className="fa-solid fa-pencil dark:text-principal-white"></i>
-                </button> 
-                      )}
+                      <div className="edit-icons-container">
+                        {hoveredUserId === usuario.id && (
+                          <>
+                            <button onClick={() => handleEdit(usuario)}>
+                              <i className="fa-solid fa-pencil dark:text-principal-white dark:hover:text-color-primary hover:text-color-primary me-5"></i>
+                            </button>
+                            <button onClick={() => handleDelete(usuario.id)}>
+                              <i className="fa-solid fa-trash dark:text-principal-white hover:text-red-500 dark:hover:text-red-500 "></i>
+                            </button>
+                          </>
+                        )}
+                      </div>
                     </td>
                   </tr>
                 ))}
@@ -188,103 +247,129 @@ const [usuarios , setUsuarios] = useState([])
             </table>
             <div className="d-flex justify-center mt-5">
 
-            <ReactPaginate 
-  previousLabel={"Anterior"}
-  nextLabel={"Siguiente"}
-  breakLabel={"..."}
-  breakClassName={"break-me"}
-  pageCount={pageCount}
-  marginPagesDisplayed={2}
-  pageRangeDisplayed={5}
-  onPageChange={handlePageClick}
-  containerClassName={"pagination"}
-  activeClassName={"active dark:text-principal-white"}  // Clase para la página activa
-  pageClassName={"custom-page-class dark:text-principal-white"}  // Clase para las páginas
-  previousClassName={"custom-previous-class dark:text-principal-white"}  // Clase para "Anterior"
-  nextClassName={"custom-next-class dark:text-principal-white"}  // Clase para "Siguiente"
-/>
+              <ReactPaginate
+                previousLabel={"Anterior"}
+                nextLabel={"Siguiente"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                activeClassName={"active dark:text-principal-white"}  // Clase para la página activa
+                pageClassName={"custom-page-class dark:text-principal-white"}  // Clase para las páginas
+                previousClassName={"custom-previous-class dark:text-principal-white"}  // Clase para "Anterior"
+                nextClassName={"custom-next-class dark:text-principal-white"}  // Clase para "Siguiente"
+              />
             </div>
           </div>
         </div>
       </div>
-     
-{/* Modal para editar usuario */}
-<Modal show={showModal} onHide={handleCloseModal} className="text-center ">
-      <Modal.Header className="dark:bg-dark-black" closeButton>
-        <Modal.Title className="dark:bg-dark-black">Editar Usuario</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="dark:bg-dark-black">
-        {/* Formulario para editar el usuario */}
-        {editUser && (
-          <form className=" dark:text-principal-white">
-            <div className="mb-3 text-start">
-              <label htmlFor="nombre" className="form-label text-dark-black">
-                Nombre
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="nombre"
-                value={editUser.nombre}
-                onChange={(e) => setEditUser({ ...editUser, nombre: e.target.value })}
-              />
-            </div>
-            <div className="mb-3 text-start">
-              <label htmlFor="apellido" className="form-label text-dark-black">
-                Apellido
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="apellido"
-                value={editUser.apellido}
-                onChange={(e) => setEditUser({ ...editUser, apellido: e.target.value })}
-              />
-            </div>
-            <div className="mb-3 text-start">
-              <label htmlFor="email" className="form-label text-dark-black">
-                Correo Electrónico
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                value={editUser.email}
-                onChange={(e) => setEditUser({ ...editUser, email: e.target.value })}
-              />
-            </div>
-            <div className="mb-3 text-start">
-              <label htmlFor="rol" className="form-label  text-dark-black">
-                Rol
-              </label>
-              <select
-                className="form-select"
-                id="rol"
-                value={editUser.rol_id}
-                onChange={(e) => setEditUser({ ...editUser, rol_id: parseInt(e.target.value) })}
-              >
-                {roles.map((role) => (
-                  <option key={role.id} value={role.id}>
-                    {role.label}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </form>
-        )}
-      </Modal.Body>
-      <Modal.Footer className="dark:bg-dark-black">
-        <Button variant="secondary" onClick={handleCloseModal}>
-          Cerrar
-        </Button>
-        <Button variant="primary" onClick={handleSaveChanges}>
-          Guardar Cambios
-        </Button>
-      </Modal.Footer>
-    </Modal>
 
-     
-      
+      {/* Modal para editar usuario */}
+      <Modal show={showModal} onHide={handleCloseModal} className="text-center ">
+        <Modal.Header className="dark:bg-dark-black" closeButton>
+          <Modal.Title className="dark:bg-dark-black">Editar Usuario</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="dark:bg-dark-black">
+          {/* Formulario para editar el usuario */}
+          {editUser && (
+            <form className=" dark:text-principal-white">
+              <div className="mb-3 text-start">
+                <label htmlFor="nombre" className="form-label text-dark-black">
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="nombre"
+                  value={editUser.nombre}
+                  onChange={(e) => setEditUser({ ...editUser, nombre: e.target.value })}
+                />
+              </div>
+              <div className="mb-3 text-start">
+                <label htmlFor="apellido" className="form-label text-dark-black">
+                  Apellido
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="apellido"
+                  value={editUser.apellido}
+                  onChange={(e) => setEditUser({ ...editUser, apellido: e.target.value })}
+                />
+              </div>
+              <div className="mb-3 text-start">
+                <label htmlFor="email" className="form-label text-dark-black">
+                  Correo Electrónico
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  value={editUser.email}
+                  onChange={(e) => setEditUser({ ...editUser, email: e.target.value })}
+                />
+              </div>
+              <div className="mb-3 text-start">
+                <label htmlFor="password" className="form-label text-dark-black">
+                  Contraseña
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value) }
+                />
+              </div>
+              <div className="mb-3 text-start">
+                <label htmlFor="confirmPassword" className="form-label text-dark-black">
+                  Confirmar Contraseña
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3 text-start">
+                <label htmlFor="rol" className="form-label  text-dark-black">
+                  Rol
+                </label>
+                <select
+                  className="form-select"
+                  id="rol"
+                  value={editUser.rol_id}
+                  onChange={(e) => setEditUser({ ...editUser, rol_id: parseInt(e.target.value) })}
+                >
+                  {roles.map((role) => (
+                    <option key={role.id} value={role.id}>
+                      {role.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </form>
+          )}
+        </Modal.Body>
+        <Modal.Footer className="dark:bg-dark-black">
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Cerrar
+          </Button>
+          <Button variant="primary" onClick={handleSaveChanges}>
+            Guardar Cambios
+          </Button>
+
+        </Modal.Footer>
+      </Modal>
+
+
+
 
     </div>
   );
--- front/src/front/js/store/flux.js
+++ front/src/front/js/store/flux.js
@@ -380,6 +380,34 @@ const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
               console.error(error);
               return { success: false, error: "Error de red" };
           }
+        },
+        deleteUser: async(idUser) =>{
+          const url = `${backendUrl}/usuarios/${idUser}`;
+          const { token } = getStore();
+          const options = {
+              method: "DELETE",
+              
+              headers: {
+
+                  "Authorization": 'Bearer ' + token
+              }
+          };
+      
+          try {
+              const resp = await fetch(url, options);
+              if (resp.status === 200) {
+                  const data = await resp.json(); 
+                  return { success: true, message :  "Usuario Eliminado exitosamente" };
+
+              } else {
+                  const errorData = await resp.json();
+                  console.log(errorData)
+                  return { success: false, error: errorData.error || "Error al Eliminar el Usuario" };
+              }
+          } catch (error) {
+              console.error(error);
+              return { success: false, error: "Error de red" };
+          }
         }
 
       },
